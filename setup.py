4fdc3ae6d3e602174583ada783722beacf3de137
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2025-03-31 16:26:19 +0500

    updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="json-db",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Use json file as database",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sinofarmonovzfkrvjl/JsonDB",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
